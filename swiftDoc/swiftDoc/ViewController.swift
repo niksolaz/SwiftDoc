5c9e74062d82dabfc40c9fb951005c72def88854
niksolaz <solazzo.nicola@gmail.com>
2017-12-12 15:34:42 +0100

    Added test for connection DB

--- swiftDoc/swiftDoc/ViewController.swift
+++ swiftDoc/swiftDoc/ViewController.swift
@@ -11,23 +11,38 @@ import Firebase
 import WebKit
 
 class ViewController: UIViewController {
-
+/*
+     Create costant to connect database
+     where Database.database().reference() reference "nameApp-****.firebaseio.com/"
+     and .child(pathString) is /name databse
+     
+ */
     let todolist_DB_Ref: DatabaseReference = Database.database().reference().child("todolistSwift")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //call login anonymous
         loginAnonymous()
         
+        //test connection database
+        //add item with child test
+        let item = todolist_DB_Ref.child("test")
+        //add value set
+        let values: [String: Any] = ["name":"test connection"]
+        //set item with value
+        item.setValue(values)
         
     }
-    
+    //Create login anonymous to firebase
     func loginAnonymous(){
         Auth.auth().signInAnonymously { (User, Error) in
             if let error = Error {
+                //in case Error
                 print(error)
             } else {
                 if let user = User {
+                    //in case Correct Login
                     print(user.uid)
                 }
             }
